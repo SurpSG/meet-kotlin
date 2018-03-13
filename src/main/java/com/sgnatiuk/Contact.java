0972b9c3659c92854604c11429b1adee28ecad7b
Sergiy Gnatyuk <surpsg2012@gmail.com>
2018-03-13 15:43:25 +0200

    added Contact as data class skeleton

---
+++ src/main/java/com/sgnatiuk/Contact.java
@@ -0,0 +1,10 @@
+package com.sgnatiuk;
+
+import java.util.Date;
+
+public class Contact {
+    private String name;
+    private String phoneNumber;
+    private Date dateOfBirth;
+    private String address;
+}
