7c93124f6bdfd3847d9457c2e9f973e83c73447e
Sergiy Gnatyuk <surpsg2012@gmail.com>
2018-03-13 16:01:29 +0200

    converted pojo class to kotlin data class

---
+++ src/main/java/com/sgnatiuk/Contact.kt
@@ -0,0 +1,10 @@
+package com.sgnatiuk
+
+import java.util.*
+
+data class Contact(
+        val name: String,
+        val phoneNumber: String,
+        val dateOfBirth: Date,
+        val address: String
+)
