444f6bf5e9c95f5d81800c8f2f425739cb102ef9
Sergiy Gnatyuk <surpsg2012@gmail.com>
2018-03-13 16:26:59 +0200

    refactored Variables

--- src/main/java/com/sgnatiuk/Variables.kt
+++ src/main/java/com/sgnatiuk/Variables.kt
@@ -1,21 +1,7 @@
 package com.sgnatiuk
 
 fun main(args: Array<String>) {
-    //final NonNullable
-    val name : String
-    val property : String? = System.getProperty("name")
-    if(property == null){
-        name = "Kotlin"
-    }else{
-        name = property
-    }
-
-    // non final, NonNullable
-    var result : String = "Hello "
-    //result = null - Not allowed
-
-    result += name // string template
-
-    println(result)
+    val name = System.getProperty("name") ?: "Kotlin"
+    println("Hello $name")
 }
 
