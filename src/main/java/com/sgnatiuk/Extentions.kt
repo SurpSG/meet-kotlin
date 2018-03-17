50ef7c9145707c909e79142647a9dfbb44cdfda6
Sergiy Gnatyuk <surpsg2012@gmail.com>
2018-03-17 17:06:52 +0200

    updated hello example

--- src/main/java/com/sgnatiuk/Extentions.kt
+++ src/main/java/com/sgnatiuk/Extentions.kt
@@ -7,6 +7,7 @@ fun main(args: Array<String>) {
     val bigIntVal = BigInteger.valueOf(100500)
     val bigIntVal2 = 100500.bi
 
+    println(bigIntVal == bigIntVal2)
 //    val binary: String = 15.toBin()
 }
 
---
+++ src/main/java/com/sgnatiuk/HelloWorldApp.kt
@@ -0,0 +1,27 @@
+package com.sgnatiuk
+
+fun main(args: Array<String>) {
+
+    //final NonNullable
+    val name : String
+    val property : String? = System.getProperty("name") //elvis operator
+    if(property == null){
+        name = "Kotlin"
+    }else{
+        name = property
+    }
+
+    // non final, NonNullable
+    var result : String = "Hello "
+    //result = null - Not allowed
+
+    result += name // string template
+
+    println(result)
+}
+
+
+
+//void function
+//function return value
+//single/last lambda arg
